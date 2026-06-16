fbfd1a8ccc7d2a1f4a28801edc17675f991d58f5
Mohammed Fuzail Rayyan <rayyan.haffiz@srmtech.com>
2026-06-16 10:19:37 +0530

    code updated

--- src/app.js
+++ src/app.js
@@ -9,12 +9,28 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = ["https://jdax.in", "http://localhost:5173"];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, or postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use("/api/events", eventRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
-
